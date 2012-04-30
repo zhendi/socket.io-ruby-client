c6e3aa4ee7d314c423c72e2a4c63175712e05726
Jason Ling <jason@jeyel.com>
2012-04-30 11:10:21 +0800

    1.8 fixes

--- lib/parser.rb
+++ lib/parser.rb
@@ -4,9 +4,9 @@ module Parser
   # returns hash as {type: '1', id: '1', end_point: '4', data: [{key: value}]}
   def self.decode(string)
     if pieces = string.match(@regexp)
-      {type: pieces[1], id: pieces[2], end_point: pieces[4], data: pieces[5]}
+      {:type => pieces[1], :id => pieces[2], :end_point => pieces[4], :data => pieces[5]}
     else
-      {type: '0'}
+      {:type => '0'}
     end
   end
 
